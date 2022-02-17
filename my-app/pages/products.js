7e5c6b791983a3d9aa12443389ccc63d9efd549f
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-17 08:20:24 +0200

    Fetched Data from server

--- my-app/pages/products.js
+++ my-app/pages/products.js
@@ -1,5 +1,35 @@
-function ProductsPage() {
-    return <div>Products</div>
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts
+    const res = await fetch('https://cantiin.com/api/products');
+    const products = await res.json();
+    console.log("I am sending the request");
+  
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+      props: {
+        products,
+      },
+    }
   }
+
+
+
+
+
+function ProductsPage({products}) {
+    return (
+        <ul>
+        {products.results.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+        );
+  }
+
+
+
+
+
   
 export default ProductsPage
