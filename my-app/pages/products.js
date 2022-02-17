2776ef28189e3e88d93f39c44f596a9e7c8945c3
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-17 09:51:30 +0200

    Products/[id].js created

--- my-app/pages/products.js
+++ my-app/pages/products.js
@@ -2,7 +2,7 @@ export async function getStaticProps() {
     // Call an external API endpoint to get posts
     const res = await fetch('https://cantiin.com/api/products');
     const products = await res.json();
-    console.log("I am sending the request");
+    //console.log("I am sending the request");
   
     // By returning { props: { posts } }, the Blog component
     // will receive `posts` as a prop at build time
