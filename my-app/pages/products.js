7bae734ed55c08b09d3d21ff016f94a0bdb66c88
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-19 15:29:05 +0200

    Understood NextJS Rendering

--- my-app/pages/products.js
+++ my-app/pages/products.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 
-
+/*
   class Page extends React.Component {
     static async getInitialProps(ctx) {
       const page = ctx.query.page || "1";
@@ -28,10 +28,43 @@ import React from "react";
         )
     }
   }
+*/
+
+
+
+  export async function getServerSideProps(ctx) {
+    // Call an external API endpoint to get posts
+    const page = ctx.query.page || "1";
+    const res = await fetch(`https://cantiin.com/api/products/?page=${page}`);
+    const products = await res.json()
+  
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+      props: {
+        products:products.results,
+      },
+    }
+  }
 
 
 
 
+  function ProductsPage({ products }) {
+    return (
+      <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+            
+            <a href={`/products/${product.id}`}>
+
+            {product.name}
+            </a>
+            </li>
+      ))}
+    </ul>
+    )
+  }
 
   
-export default Page
+export default ProductsPage
