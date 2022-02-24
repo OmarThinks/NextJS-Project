9bfbb40fcd247e27823204ccc136adf805c2d435
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-24 07:46:05 +0200

    made _app.js change with getLayout

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -2,12 +2,8 @@ import '../styles/globals.css'
 import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-  
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
