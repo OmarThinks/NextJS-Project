fe7afbfb19b792f047f6ba961a6a153d66d37034
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-28 06:27:25 +0200

    Added a form

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -12,6 +12,15 @@ export default function Home() {
         <a href='/products'>Products List</a>
         <div><Image src="/images/dummy_image.png" alt="me" width="64" height="64" /></div>
         <div><img src="/images/dummy_image.png" /></div>
+
+
+        <form action="/send-data-here" method="post">
+  <label for="first">First name:</label>
+  <input type="text" id="first" name="first" />
+  <label for="last">Last name:</label>
+  <input type="text" id="last" name="last" />
+  <button type="submit">Submit</button>
+</form>
     </div>
   )
 }
